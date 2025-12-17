203b237a4ebf63bd0517eefecc910af3c75a20b1
renan <renanzin007@icloud.com>
2025-12-17 12:08:06 -0300

    first push

---
+++ detections/face_detection.py
@@ -0,0 +1,26 @@
+import cv2
+
+cap=cv2.VideoCapture(0)
+frontalface_detection=cv2.CascadeClassifier("../haarcascade/haarcascade_frontalface_default.xml")
+
+
+while True:
+    ret, frame=cap.read()
+    video=cv2.flip(frame,1)
+    gray_video=cv2.cvtColor(video,cv2.COLOR_BGR2GRAY)
+
+    face=frontalface_detection.detectMultiScale(gray_video,1.2,6)
+    for (x,y,w,h) in face:
+        cv2.rectangle(video,(x,y),(x+w,y+h),(0,0,255),5)
+        print(int(x+w),int(y+h))
+
+    cv2.imshow('face_detection', video)
+
+    key = cv2.waitKey(15) & 0xff
+    if key == 'q':
+        break
+                              
+cv2.destroyAllWindows()
+
+
+
